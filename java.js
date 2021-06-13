0ad8c993410aaa457ca8f891f37b385adae84edd
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-06-13 17:31:29 -0300

    js

--- java.js
+++ java.js
@@ -12,8 +12,17 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav-toggle', 'nav-menu')
- 
 
 
+
+/*===== REMOVER MENU  =====*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
