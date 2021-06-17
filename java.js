cd99becd446091c8d260422f5d64c14ccff8d8c4
Chris Maracujá <74564883+Maracujacake@users.noreply.github.com>
2021-06-17 16:07:06 -0300

    jsssssss

--- java.js
+++ java.js
@@ -47,3 +47,12 @@ function scrollActive(){
     })
 }
 window.addEventListener('scroll', scrollActive)
+
+
+// ===== MUDAR O BACKGROUND [HEADER] =====
+
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
